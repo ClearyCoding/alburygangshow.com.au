7cf2997d21b457fd3008e641fc31ac8bf9920cf3
Alexander Cleary <alexjacleary@gmail.com>
2023-10-05 14:47:18 +1100

    Removed Mojangles Font, Added Splashes

--- assets/scripts/billboard-splash.js
+++ assets/scripts/billboard-splash.js
@@ -1,6 +1,6 @@
 const subtitleSplashes = [
     'Est. 1965',
-    'Harley Here From First National Real Estate',
+    '\"Harley here from First National Real Estate\"',
     'NOOOOOAH',
     '*Laughs Maniacally*',
     'Alexander Hamilton!',
@@ -12,6 +12,28 @@ const subtitleSplashes = [
     'Gangity Gangiest Gang Show Gang',
     'Cool & Creamy',
     'Super-Trustworthy Steve & Drew Studios',
-    'OH NO! I\'ve lost my Mojo!'
+    'OH NO! I\'ve lost my Mojo!',
+    'I\'ve got a dream!',
+    'For the Republic, FOR DEMOCRACY!',
+    '1300655506',
+    'Flying Monkeys!',
+    'The Bird Is The Word',
+    'Don\'t disturb the costume department!',
+    'Check out the RAG!',
+    'DEFINITELY not a cult...',
+    'Sorry, need to edit the script again...',
+    'Making kids voices hurt since \'65',
+    'Birds of a Feather!',
+    'This is the best... This is the best night...',
+    'Encore!',
+    'What were the moves to Macarena again??',
+    'Wait! Why are we different cast members?',
+    '\"GO TO BED\" - Drew',
+    'Better than (insert show name)!',
+    'Free Range Cast!',
+    'Thx 2nd Albury!',
+    '\"Albury Town Is Our Home!\"',
+    "TWO, THREE, FOUR!",
+    'You were AMAZING!'
 ]
 document.querySelector('#pageHome-section-billboard-container-splash-text').innerText = subtitleSplashes[Math.floor(Math.random() * subtitleSplashes.length)]
