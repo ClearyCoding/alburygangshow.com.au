5129b09a282e007d9ed2db85a4043a94736fa831
Alexander Cleary <alexjacleary@gmail.com>
2023-10-05 13:39:10 +1100

    More Splash Texts

--- assets/scripts/billboard-splash.js
+++ assets/scripts/billboard-splash.js
@@ -6,6 +6,12 @@ const subtitleSplashes = [
     'Alexander Hamilton!',
     'You’re an actor, ACT!',
     'My Daddy\'s gonna hear about this!',
-    'Don\'t forget to SMILE'
+    'Don\'t forget to SMILE',
+    'Petition to burn the tight pants...',
+    'Dehydrated Water!',
+    'Gangity Gangiest Gang Show Gang',
+    'Cool & Creamy',
+    'Super-Trustworthy Steve & Drew Studios',
+    'OH NO! I\'ve lost my Mojo!'
 ]
 document.querySelector('#pageHome-section-billboard-container-splash-text').innerText = subtitleSplashes[Math.floor(Math.random() * subtitleSplashes.length)]
