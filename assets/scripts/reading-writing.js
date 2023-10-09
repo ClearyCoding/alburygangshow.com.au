80f958ffc12cf9261b477f97c5fed7ae0aa9a085
Alexander Cleary <alexjacleary@gmail.com>
2023-10-10 09:17:30 +1100

    Added Extension To Reading Writing Hotline Easter Egg

--- assets/scripts/reading-writing.js
+++ assets/scripts/reading-writing.js
@@ -1,6 +1,7 @@
 const hotline = ["1", "3", "0", "0", "6", "5", "5", "5", "0", "6"];
 const readingWritingAudio = new Audio('/assets/audio/reading_writing.mp3');
 let currentDigit = 0;
+let readingWritingCount = 0;
 document.body.addEventListener('keydown', (event) => {
     if (event.key === hotline[currentDigit]) {
         currentDigit++;
@@ -13,6 +14,14 @@ document.body.addEventListener('keydown', (event) => {
 })
 
 function readingWriting() {
-
+    currentDigit = 0;
+    readingWritingCount++;
+    if (readingWritingCount >= 3){
+        if (screen.width > 500) {
+            window.location.href="https://www.readingwritinghotline.edu.au/";
+        } else {
+            window.location.href="Tel: 1300655506 ";
+        }
+    }
     readingWritingAudio.play();
 }
