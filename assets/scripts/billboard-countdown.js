293516f4a36d706ca15e89f0857b205c0b2442f3
Alexander Cleary <alexjacleary@gmail.com>
2024-08-11 23:06:27 +1000

    Replaced VARs in Countdown with CONSTs and LETs

--- assets/scripts/billboard-countdown.js
+++ assets/scripts/billboard-countdown.js
@@ -1,23 +1,23 @@
 // CREDIT: https://www.w3schools.com/howto/howto_js_countdown.asp
 
 // Set the date we're counting down to
-var countDownDate = convertTZ("Aug 17, 2024 16:00:00", "Australia/Sydney");
+const countDownDate = convertTZ("Aug 17, 2024 16:00:00", "Australia/Sydney");
 console.log(countDownDate)
 
 // Update the countdown every 1 second
-var x = setInterval(function() {
+const x = setInterval(function() {
 
     // Get today's date and time
-    var now = new Date().getTime();
+    let now = new Date().getTime();
 
     // Find the distance between now and the count-down date
-    var distance = countDownDate - now;
+    let distance = countDownDate - now;
 
     // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Append 0 to front of Day counter if the day counter reaches the single-digits
     if (days < 10) {
